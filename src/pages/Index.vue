98916ef11dac7e529115ed9c8f3f54d3a7771a83
Jose Jose Rojas <Jose.Rojas@tomtom.com>
2021-03-01 21:40:12 +0100

    Updating to Loodico API KEY

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,7 +9,8 @@ import Map from 'components/Map.vue'
 import { LocalStorage } from 'quasar'
 
 // LocalStorage.set('apikey', 'Vn26cA8knt2E8sl0WBEWvAgWGRUf59mm')
-LocalStorage.set('apikey', 'y4OUatPPGQgS71stpTvqD6n2iyteZrYD')
+// LocalStorage.set('apikey', 'y4OUatPPGQgS71stpTvqD6n2iyteZrYD')
+LocalStorage.set('apikey', 'jsRs4JdL9YyeSryAvq0AFDTi2cZWjoeG')
 
 export default {
   name: 'PageIndex',
