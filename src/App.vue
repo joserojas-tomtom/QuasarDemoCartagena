06d8e95f00606866ff16f6260ee63e5ced6c19f2
Jose Jose Rojas <Jose.Rojas@tomtom.com>
2021-03-17 09:15:54 +0100

    Fixed initial loading city

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,27 @@
   </div>
 </template>
 <script>
-// import { LocalStorage } from 'quasar'
+import { LocalStorage } from 'quasar'
 import store from './router/store.js'
 
 export default {
   name: 'App',
   mounted () {
+    const getDefaultCity = async () => {
+      try {
+        const response = await fetch('db-structure.json')
+        const data = await response.json()
+        const citiesDB = data.cities
+        LocalStorage.set('citiesDB', citiesDB)
+        const currentCity = LocalStorage.getItem('currentCity')
+        if (!currentCity) {
+          LocalStorage.set('currentCity', citiesDB.cartagena)
+        }
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getDefaultCity()
     store.actions.handleAuthChanged()
   }
 }
